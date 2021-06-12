50cb09bf3ad9e103653a968aef857986eb19891c
Inoxia25 <nandinisapna1401@gmail.com>
2021-06-12 22:49:36 +0530

    added bootstrap classes and functional components

--- src/InternCard.js
+++ src/InternCard.js
@@ -1,94 +1,86 @@
 import React, { Component } from "react";
-
-export class InternCard extends Component {
-  render() {
+import { AiFillHome} from 'react-icons/ai';
+import { MdDateRange } from "react-icons/md";
+import { FaRupeeSign } from "react-icons/fa";
+import {AiOutlineArrowRight} from 'react-icons/ai';
+import {IoIosArrowForward } from "react-icons/io";
+export default function InternCard(props) {
     return (
-      <div class="internship">
-        <div class="individual_internship_header">
-          <div class="internship_logo">
-            <img
-              loading="lazy"
-              src="/E-cell_White-02.png"
-              class="logo company_logo"
-            ></img>
-          </div>
-          <div class="company">
-            <div class="profile">
-              <a href="" class="profile_name">
-                {this.props.company}
-              </a>
+        <div className="internship">
+            <div className="individual_internship_header d-flex flex-row justify-content-start">
+                <div className="internship_logo">
+                    <img loading="lazy" src="/E-cell_White-02.png" className="logo  ml-4 company_logo" />
+                </div>
+                <div className="company">
+                    <div className="mb-2">
+                        <a href="" className="profile_name">
+                            {props.company}
+                        </a>
+                    </div>
+                    <div className="company_name">
+                        <a className="link_display_like_text" href="">
+                            {props.profile}{" "}
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="company_name">
-              <a class="link_display_like_text" href="">
-                {this.props.profile}{" "}
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="individual_internship_details  individual_internship_internship">
-          <div id="location_names">
-            <span class="material-icons md-18 md-dark">home</span>
+            <div className="mb-2 mt-3 individual_internship_internship">
+                <div id="location_names" className="d-flex flex-row mb-4 mt-4">
+                    <span><AiFillHome  className="material-icons md-18 md-dark"/></span>
+                
+                    <span>
+                        <a className="location_link ml-1" href="/">
+                            Remote
+                        </a>{" "}
+                    </span>
+                </div>
+                <div className="internship_other_details_container mt-2 d-flex flex-row justify-content-start">
+                    <div className="other_detail_item_row d-flex flex-row">
+                        <div className="other_detail_item ">
+                            <div className="item_heading">
+                                <span className="material-icons md-18 md-dark md-icon"><MdDateRange/></span>
+                                
+                                <span className="md-dark icon-text">Duration</span>
+                            </div>
+                            <div className="item_body d-flex flex-row">{props.duration} </div>
+                        </div>
+                    </div>
 
-            <span>
-              <a class="location_link" href="/">
-                Remote
-              </a>{" "}
-            </span>
-          </div>
-          <div class="internship_other_details_container">
-            <div class="other_detail_item_row">
-              <div class="other_detail_item ">
-                <div class="item_heading">
-                  <span class="material-icons md-18 md-dark md-icon">
-                    date_range
-                  </span>
-                  <span class="md-dark icon-text">Duration</span>
+                    <div className="other_detail_item_row d-flex flex-row">
+                        <div className="other_detail_item  stipend_container">
+                            <div className="item_heading">
+                                <span className="material-icons  md-18 md-dark md-icon"> <FaRupeeSign/></span>
+                                <span className="md-dark icon-text">Stipend</span>
+                              
+                            </div>
+                            <div className="item_body d-flex flex-row">
+                                <span className="stipend">
+                                    <i className="ic-16-currency-inr"></i> {props.stipend}
+                                </span>{" "}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="other_detail_item_row d-flex flex-row">
+                        <div className="other_detail_item  mr-5 apply_by">
+                            <div className="item_heading">
+                                <span className="material-icons md-18 md-dark md-icon">access_time</span>
+                                <span className="md-dark icon-text">Apply By</span>
+                            </div>
+                            <div className="item_body d-flex flex-row">{props.last_date}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="item_body">{this.props.duration} </div>
-              </div>
             </div>
-
-            <div class="other_detail_item_row">
-              <div class="other_detail_item  stipend_container">
-                <div class="item_heading">
-                  <span class="material-icons  md-18 md-dark md-icon">
-                    local_atm
-                  </span>
-                  <span class="md-dark icon-text"> Stipend </span>
+            <div className="button_container mt-4 d-flex flex-row justify-content-between">
+                <div className="tags_container">
+                    <div className="label_container label_container_desktop">Internship </div>
                 </div>
-                <div class="item_body">
-                  <span class="stipend">
-                    <i class="ic-16-currency-inr"></i> {this.props.stipend}
-                  </span>{" "}
-                </div>
-              </div>
+                <a className="view_detail_button mr-2 float-right" href="">
+                    <span className="md-18"> Know More</span>
+                    <span className="material-icons md-icon"><IoIosArrowForward/></span>
+                </a>
             </div>
-            <div class="other_detail_item_row">
-              <div class="other_detail_item  apply_by">
-                <div class="item_heading">
-                  <span class="material-icons  md-18 md-dark md-icon">
-                    access_time
-                  </span>
-                  <span class="md-dark icon-text">Apply By</span>
-                </div>
-                <div class="item_body">{this.props.last_date}</div>
-              </div>
-            </div>
-          </div>
         </div>
-        <div class="button_container">
-          <div class="tags_container">
-            <div class="label_container label_container_desktop">
-              Internship{" "}
-            </div>
-          </div>
-          <a class="view_detail_button" href="">
-            <span class="md-18"> Know More</span>
-            <span class="material-icons md-icon">chevron_right</span>
-          </a>
-        </div>
-      </div>
     );
-  }
 }
-export default InternCard;
+
